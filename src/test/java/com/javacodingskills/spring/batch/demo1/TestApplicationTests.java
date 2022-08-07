c8d240a731967afc5c3d4770255a567a99edcf8a
sudha <guragolsudha@gmail.com>
2022-08-07 17:56:40 +0530

    first commit

---
+++ src/test/java/com/javacodingskills/spring/batch/demo1/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacodingskills.spring.batch.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
